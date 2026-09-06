7d7ae5d0a1b89884b972d7bc33c3a87f3a11a967
chichiletsie <chichiangeloletsie@gmail.com>
2026-09-06 14:16:32 +0200

    feat: (db) Set up database connection and session handling

---
+++ backend/app/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine # connects raw connection bewteen python and database
+from sqlalchemy.orm import declarative_base, sessionmaker
+from app.config import SQLITE_DB_PATH
+
+# SQLite-specific connect args for thread safety in multi-threaded environments
+engine = create_engine(
+    SQLITE_DB_PATH,
+    connect_args={"check_same_thread": False},
+    echo=False
+)
+
+#to open and close temporary work sessions whenever code needs to read or write data.
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base() #define database tables as Python classes.
+
+#gives each incoming web request its own database session and guarantees that session closes when the request is done.
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
